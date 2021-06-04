de7aeadd18f82faa930761fc1b3ebbd8cae3605f
sanaevvv <sanaevvv@gmail.com>
2021-06-04 19:22:52 +0900

    TS追加

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,7 +6,7 @@ import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
@@ -23,7 +23,7 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as { date: string; title: string})
   };
 }
 
@@ -69,7 +69,7 @@ export function getSortedPostsData() {
     // データを id と合わせる
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string}),
     };
   });
   // 投稿を日付でソートする
